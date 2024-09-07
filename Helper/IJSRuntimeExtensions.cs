f309755fd046ffae4102b9d3d164ec9011ba3d9b
Krešimir Wittenberg <kresimir.wittenberg@gmail.com>
2024-09-08 00:36:35 +0200

    update

---
+++ Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper;
+
+public static class IJSRuntimeExtensions
+{
+    public static async Task ToastrSuccess(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showToastr", "success", message);
+    }
+
+    public static async Task ToastrError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+    }
+
+    public static async Task SweetalertSuccess(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showSwal", "success", message);
+    }
+
+    public static async Task SweetalertError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showSwal", "error", message);
+    }
+}
